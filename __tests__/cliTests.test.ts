097d5b27419de107dfa10aa09feef34432199c95
Lukas Müller <WookieFPV@users.noreply.github.com>
2025-04-27 11:44:20 +0200

    chore: add e2e tests (using the bsky app as an example)

---
+++ __tests__/cliTests.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, it } from "bun:test";
+import { $ } from "zx";
+
+/**
+ * These E2E Test use a real package json + app.config.js and a node_modules folder where everything is removed expect the config plugins files (this way CI is much faster because we can add those files to git instead of installing packages)
+ */
+describe("CLI Tests", () => {
+    it("should show the correct config plugins", async () => {
+        const { stdout: node } = await $`node -v`;
+        const { stdout: lcp } = await $`npx list-config-plugins@latest -v`;
+        console.log(`node ${node.trim()} list-config-plugins: ${lcp.trim()}`);
+
+        const { stdout } = await $`npx list-config-plugins@latest`;
+        expect(stdout).toMatchSnapshot();
+    });
+});
